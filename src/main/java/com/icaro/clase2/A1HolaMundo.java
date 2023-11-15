a40b7871cbf4e4b3f1904cd5660d39273f1bfde0
Carlos Yanes <carlosayanesp@gmail.com>
2023-11-15 17:05:24 -0300

    Ejemplo clase 2

---
+++ src/main/java/com/icaro/clase2/A1HolaMundo.java
@@ -0,0 +1,17 @@
+package com.icaro.clase2;
+
+public class A1HolaMundo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo!"); //String
+        System.out.println(10);            //Entero
+        System.out.println(20.56);         //Flotante
+        System.out.println(true);          //Booleano
+        System.out.println('C');           //Caracter o Char
+
+        System.out.println("Hola " + "Mundo, hoy es: " + 31);
+
+    }
+
+}
---
+++ src/main/java/com/icaro/clase2/A2ImprimirSemana.java
@@ -0,0 +1,14 @@
+package com.icaro.clase2;
+
+public class A2ImprimirSemana {
+
+    public static void main(String[] args) {
+        System.out.println("Día 1 - Lunes");
+        System.out.println("Día 1 - Martes");
+        System.out.println("Día 1 - Miércoles");
+        System.out.println("Día 1 - Jueves");
+        System.out.println("Día 1 - Viernes");
+        System.out.println("Día 1 - Sábado");
+        System.out.println("Día 1 - Domingo");
+    }
+}
